d0fa238810ee1c17a06adc6a3df887e59b2839eb
sarahbzm22 <sbzm22abril@gmail.com>
2025-11-03 04:23:17 +0100

    ya casi terminando

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* Abre el menú */
+// Abre el menú
 function openMenu() {
   console.log("Función openMenu");
   document.getElementById(
@@ -6,7 +6,7 @@ function openMenu() {
   ).firstElementChild.lastElementChild.style.right = "0vw";
 }
 
-/* Cierra el menú */
+// Cierra el menú
 function closeMenu() {
   console.log("Función closeMenu");
   document.getElementById(
@@ -32,21 +32,26 @@ document.addEventListener("DOMContentLoaded", () => {
   }, 2000);
 });
 
-/* Abre la ventana modal */
+//Inicializa AOS
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init({
+    duration: 1500,
+  });
+  document.getElementById("only-section").classList.add("visible");
+});
+
+
+// Abre la ventana modal de la galería
 function openModal(figura) {
   console.log("Función openModal");
   //Buscamos la ventana modal
-  var modal = document.getElementById("modal");
+  var modal = document.getElementById("modal-gallery");
   //Cogemos la imagen para la ventana modal
   var rutaImagen = figura.firstElementChild.getAttribute("src");
   console.log("valor de la ruta de la imagen: " + rutaImagen);
   var pieImagen = figura.lastElementChild.innerHTML; //Cogemos el figcaption completo
   console.log("Pie de imagen: " + pieImagen);
 
-  //Primera opción para cambiar el atributo src de la imagen de la ventana modal
-  //modal.querySelector("img").src = rutaimagen;
-
-  //Segunda opción
   modal.firstElementChild.firstElementChild.setAttribute("src", rutaImagen);
 
   //Cambiamos el valor del figcaption con la primera opción
@@ -55,10 +60,73 @@ function openModal(figura) {
   modal.style.display = "flex";
 }
 
-/* Cierra la ventana modal */
+// Cierra la ventana modal
 function closeModal() {
   console.log("Función openModal");
   //Buscamos la ventana modal
-  var modal = document.getElementById("modal");
+  var modal = document.getElementById("modal-gallery");
   modal.style.display = "none";
+}
+
+// Contador del precio
+let ticketCount = 1;
+let ticketPrice = 0;
+const maxTickets = 10;
+
+function detectTicketType() {
+  // Detectamos según el nombre del archivo
+  const path = window.location.pathname;
+
+  if (path.includes("buying_section_2.html")) {
+    ticketPrice = 300;
+  } else {
+    ticketPrice = 150;
+  }
+
+  updateDisplay();
+}
+
+function changeNumber(amount) {
+  const numberDisplay = document.getElementById("number-of-tickets");
+  ticketCount += amount;
+  if (ticketCount < 1) ticketCount = 1;
+  if (ticketCount > maxTickets) ticketCount = maxTickets;
+  numberDisplay.textContent = ticketCount;
+  updateDisplay();
+}
+
+function updateDisplay() {
+  const priceDisplay = document.getElementById("price");
+  priceDisplay.textContent = (ticketCount * ticketPrice).toFixed(2);
+  
+}
+
+document.addEventListener("DOMContentLoaded", detectTicketType);
+
+// Abrimos la ventana modal
+function openTicketsModal() {
+  console.log("Abriendo modal de tickets");
+
+  //Tomamos los valores del formulario
+  const name = document.getElementById("name").value;
+  const surname = document.getElementById("surname").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone-number").value;
+
+  //Los mostramos en el modal
+  document.getElementById("n").textContent = name;
+  document.getElementById("sn").textContent = surname;
+  document.getElementById("eml").textContent = email;
+  document.getElementById("phn").textContent = phone;
+
+  //Mostramos el modal
+  document.getElementById("modal-tickets").style.display = "flex";
+
+  //Evitamos que el formulario recargue la página
+  return false;
+}
+
+function closeTicketsModal() {
+  console.log("Cerrando modal de tickets");
+  document.getElementById("modal-tickets").style.display = "none";
 }
